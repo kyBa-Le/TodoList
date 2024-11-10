b1a71918aeb7ff2f1d25bf6b957f4c52614dbb3b
kyBa-Le <lekyba2000hc@gmail.com>
2024-11-10 22:35:29 +0700

    8 | create feature search tasks by title

--- src/main/java/com/example/todolist/API/Restful/Dto/Response/TaskResponse.java
+++ src/main/java/com/example/todolist/API/Restful/Dto/Response/TaskResponse.java
@@ -1,9 +1,24 @@
 package com.example.todolist.API.Restful.Dto.Response;
 
+import com.example.todolist.Domain.Entity.Task;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public record TaskResponse(
         String id,
         String title,
         String description,
         String userId
 ) {
+    public static List<TaskResponse> FromTasksToDtoResponses(List<Task> tasks) {
+        return tasks.stream()
+                .map(task -> new TaskResponse(
+                        task.getId(),
+                        task.getTitle(),
+                        task.getDescription(),
+                        task.getUserId()
+                ))
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/example/todolist/Infrastructure/Repository/TaskRepository.java
+++ src/main/java/com/example/todolist/Infrastructure/Repository/TaskRepository.java
@@ -1,12 +1,15 @@
 package com.example.todolist.Infrastructure.Repository;
 
 import com.example.todolist.Domain.Entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface TaskRepository extends PagingAndSortingRepository<Task, String> {
     void save(Task task);
     Task findById(String id);
+    Page<Task> findTaskByTitleContaining(String title, Pageable pageable);
 
     @Query("select t from Task t  join fetch t.user where t.id = :id")
     Task findByIdWithUser(String id);
