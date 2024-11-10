9a5fedaa435980bf586b0d20d724b3eb4b10f088
kyBa-Le <lekyba2000hc@gmail.com>
2024-11-10 15:56:48 +0700

    11 | create feature get all tasks with pagination

--- src/main/java/com/example/todolist/Infrastructure/Repository/TaskRepository.java
+++ src/main/java/com/example/todolist/Infrastructure/Repository/TaskRepository.java
@@ -1,12 +1,19 @@
 package com.example.todolist.Infrastructure.Repository;
 
 import com.example.todolist.Domain.Entity.Task;
+import io.micrometer.common.lang.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface TaskRepository extends PagingAndSortingRepository<Task, String> {
     void save(Task task);
     Task findById(String id);
+
+    @Override
+    @NonNull
+    Page<Task> findAll(@NonNull Pageable pageable);
 
     @Query("select t from Task t  join fetch t.user where t.id = :id")
     Task findByIdWithUser(String id);
