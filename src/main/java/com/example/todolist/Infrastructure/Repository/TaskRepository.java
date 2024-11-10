0841f001b8bb5c6f6a052e2c2728b2a508d829d5
kyBa-Le <lekyba2000hc@gmail.com>
2024-11-10 19:30:35 +0700

    11 | fix code review 1.1

--- src/main/java/com/example/todolist/Infrastructure/Repository/TaskRepository.java
+++ src/main/java/com/example/todolist/Infrastructure/Repository/TaskRepository.java
@@ -1,19 +1,12 @@
 package com.example.todolist.Infrastructure.Repository;
 
 import com.example.todolist.Domain.Entity.Task;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.lang.NonNull;
 
 public interface TaskRepository extends PagingAndSortingRepository<Task, String> {
     void save(Task task);
     Task findById(String id);
-
-    @Override
-    @NonNull
-    Page<Task> findAll(@NonNull Pageable pageable);
 
     @Query("select t from Task t  join fetch t.user where t.id = :id")
     Task findByIdWithUser(String id);
